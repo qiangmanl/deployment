826fda9a56d48de762e760abc7bcb48dc4c1c732
qiangmanl <104294471+qiangmanl@users.noreply.github.com>
2023-02-05 13:56:43 +0800

    Create clash_run.sh

---
+++ clash_run.sh
@@ -0,0 +1,6 @@
+echo defaut http,https proxy ==127.0.0.1:7890
+echo defaut sock proxy == 127.0.0.1:7891
+echo defaut clash home@https://clash.razord.top/
+echo more config see the config.yaml
+echo Download newest version from https://github.com/Dreamacro/clash/releases/latest
+./clash -d .
