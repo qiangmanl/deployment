8900099ce119fabe87e5347c016010b2b3ceedb6
qiangmanl <104294471+qiangmanl@users.noreply.github.com>
2023-02-05 13:59:35 +0800

    Update clash_run.sh

--- clash_run.sh
+++ clash_run.sh
@@ -3,4 +3,5 @@ echo defaut sock proxy == 127.0.0.1:7891
 echo defaut clash home@https://clash.razord.top/
 echo more config see the config.yaml
 echo Download newest version from https://github.com/Dreamacro/clash/releases/latest
+echo prepare a available config.yaml in workdir
 ./clash -d .
